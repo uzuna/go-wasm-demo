9351594c5a8788de09bffa7664479802c6ea9c7e
Fumiya Fujinaka <uzuna.kf@gmail.com>
2018-09-06 23:26:04 +0900

    demo image renderin wasm
    - imageを使ってpngの生成ができた
    - golang向けに作ったものがそのまま使えそう

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"syscall/js"
 	"time"
+
+	"github.com/fogleman/gg"
 )
 
 func main() {
@@ -68,16 +73,52 @@ func main() {
 			counter++
 		}
 	}()
-	for {
-		time.Sleep(time.Second)
-		fmt.Println(fmt.Sprintf("Hello wasm outer loop At: %d", counter))
-		if counter > 10 {
-			fmt.Println("break outer loop")
-			break
-		}
-		counter++
+	// for {
+	// 	time.Sleep(time.Second)
+	// 	fmt.Println(fmt.Sprintf("Hello wasm outer loop At: %d", counter))
+	// 	if counter > 10 {
+	// 		fmt.Println("break outer loop")
+	// 		break
+	// 	}
+	// 	counter++
+	// }
+
+	// png rendering
+	{
+		pngb64 := Render()
+		content := js.Global().Get("document").Call("getElementById", "content")
+
+		// create div
+		img := js.Global().Get("document").Call("createElement", "img")
+		img.Set("src", "data:image/png;base64,"+pngb64)
+
+		// append child
+		content.Call("appendChild", img)
 	}
 
 	forever := make(chan bool)
 	<-forever
+}
+
+// base 64 pngの生成
+func Render() string {
+	buf := new(bytes.Buffer)
+
+	W := 100
+	H := 100
+	// var mask *image.Alpha
+
+	dc := gg.NewContext(W, H)
+	dc.DrawRectangle(25, 50, 100, 100)
+	dc.SetRGBA(255, 128, 0, 1)
+	dc.Fill()
+	dc.EncodePNG(buf)
+
+	b64 := new(bytes.Buffer)
+	encoder := base64.NewEncoder(base64.StdEncoding, b64)
+
+	io.Copy(encoder, buf)
+	encoder.Close()
+
+	return string(b64.Bytes())
 }
